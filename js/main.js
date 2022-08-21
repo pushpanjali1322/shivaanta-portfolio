f9d3bb89409bd3d7536fda7459b043423cf44b4d
PUSHPANJALI PATEL <pushpanjalipatel27@gmail.com>
2022-08-21 18:51:09 +0530

    slide show

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $(function(){
   $(function(){
     $("#costum-footer").load("footer.html");  
   });
+
+  function slideShowStart(x) {
+    // alert(x.id);
+    document.getElementById(x.id).setAttribute("data-bs-ride" , "carousel")
+   
+  }
+  
+  function slideShowEnd(x) {
+    document.getElementById(x.id).setAttribute("data-bs-ride" , "false")
+  }
